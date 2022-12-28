e34e7d43cf0a950a27f3c8518bef23f41fc95b4e
Virendra Kumar Shukla <107716546+Virendra3970@users.noreply.github.com>
2022-12-28 17:54:40 +0530

    Update and rename 4.cpp to Mini_Project.cpp

--- Mini_Project.cpp
+++ Mini_Project.cpp
@@ -1,9 +1,9 @@
-// Question: 
-/*Write a C++ program to design a base class Person (name,address,phone_no) Derive a class Employee(eno, ename) from Person. Derive a class Manager(designation, department name, basic-salary) from Employee. Write a menu driven program to:
- a. Accept all details of 'n' managers.
- b. Display manager having highest salary*/
- 
-// Author:- Virendra Kumar
+/*Problem Statement:
+* Write a C++ program to design a base class Person (name,address,phone_no) Derive a class Employee(eno, ename) from Person. Derive a class Manager(designation, department name, basic-salary) from Employee. Write a menu driven program to:
+* a. Accept all details of 'n' managers.
+* b. Display manager having highest salary
+*Author: Virendra Kumar
+*Date:- 28-Dec-2023
  
 #include<iostream>
 #include<cstring>
